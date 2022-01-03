410af66dc8003241ceb6ab8cfd99e80603aaa3d9
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-01-03 15:26:32 +0800

    Update emmc.m

--- emmc.m
+++ emmc.m
@@ -3,11 +3,11 @@
 插入U盘到r2c plus 开机。
 
 U盘输出路径/mnt/sda1/gujian.img  建议给固件命名gujian.img
-
+      
+eMMC唯一路径为 /dev/mmcblk0 
+      
 执行写盘
 
 dd if=/mnt/sda1/gujian.img of=/dev/mmcblk0 bs=1M 
-      
-      eMMC唯一路径为 /dev/mmcblk0 
-      
+
 写入完成后建议执行 sync 结束后可以重启拔了U盘和tf卡
